32910663a2ee781d868a9605e2941a09ce2d3188
apikozh <andrey.pikozh@gmail.com>
2014-11-22 20:39:22 +0200

    Game class added

--- src/ClientSocket.java
+++ src/ClientSocket.java
@@ -3,15 +3,43 @@ import java.io.*;
 
 public class ClientSocket extends Thread { 
 	protected Socket clientSocket;
+	private Game game;
 	
-	public ClientSocket(Socket clientSocket) {
+	public ClientSocket(Socket clientSocket, Game game) {
 		this.clientSocket = clientSocket;
+		this.game = game;
+		
 		start();
 	}
 	
 	public void run() {
-		while (true) {
-		}
+		System.out.println ("New communication thread started");
+
+		Unit unit = new Unit();
+		
+		try { 
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), false);
+			out.println("Welcome to Cool Hackathon!");
+			out.println("map 30 30");
+			
+			BufferedReader in = new BufferedReader(
+				 new InputStreamReader(clientSocket.getInputStream())); 
+
+			//String inputLine; 
+
+			while (true) {
+				String line = in.readLine();
+				Scanner cmdScanner = new Scanner(line);
+				String cmd = cmdScanner.next();
+				switch (cmd) {
+					case "name": ;
+					default: ;
+				}
+				
+				//game.addUnit(unit);
+				
+
+			}
 	}
 
 }
---
+++ src/Game.java
@@ -0,0 +1,22 @@
+
+
+class Game {
+	ArrayList<Unit> units;
+	ArrayList<Bonus> bonuses;
+	GameMap map;
+	ArrayList<Bullet> bullets;
+		
+	ArrayList<Unit> getUnits() {
+		return units;
+	}
+
+	ArrayList<Unit> getBonuses() {
+		return bonuses;
+	}
+
+	Map getMap() {
+		return map;
+	}
+
+	
+}
