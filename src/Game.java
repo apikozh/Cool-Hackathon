4d935a8b1fe1ca487a0c226a2903271341267989
apikozh <andrey.pikozh@gmail.com>
2014-11-23 03:00:06 +0200

    Updated processBullets

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,7 @@ class Game {
 	private static ArrayList<Bonus> bonuses;
 	private static GameMap map;
 	private static ArrayList<Bullet> bullets;
-
+	private static ArrayList<MapObject> mapObjects;
     private static ArrayList<Team> teams;
 
 	public static ArrayList<Unit> getUnits() {
@@ -27,6 +27,8 @@ class Game {
         return map;
     }
 
+	
+	
     public static void addUnit(Unit unit) {
         int elementPositionY, elementPositionX;
         Random random = new Random();
@@ -68,13 +70,40 @@ class Game {
             if (map.getElement(elementPositionX, elementPositionY) == null) {
                 Wall wall = new Wall();
                 map.setElement(elementPositionX, elementPositionY, wall);
-                numberOfElements++;
+                mapObjects.add(wall);
+				numberOfElements++;
             }
         }
 
     }
 
-	private static void proccessBullets() {
+	private static int findBonusAt(int posX, int posY) {
+		for (int index=0; index<bonuses.size(); index++) {
+			if (bonuses.get(index).getPositionX() == posX &&
+				bonuses.get(index).getPositionY() == posY)
+			{
+				return index;
+			}
+		}
+		return -1;
+	}
+	
+	private static void processDeadUnit(Unit unit) {
+		if (unit.getLivesNumber() > 1) {
+			Random random = new Random();
+            int posX, posY;
+			do {
+				posX = random.nextInt(map.getWidth());
+				posY = random.nextInt(map.getHeight());
+			} while (map.getElement(posX, posY) != null);
+			unit.setPosition(posX, posY);
+			unit.setHealth(100);
+			unit.setLivesNumber(unit.getLivesNumber() - 1);
+		}else
+			units.remove(unit);
+	}
+	
+	private static void processBullets() {
 		Bullet bullet = null;
 		for(int index = bullets.size() - 1; index>=0; index--) {
 			bullet = bullets.get(index);
@@ -82,14 +111,45 @@ class Game {
 				bullet.setLeftDelay(bullet.getLeftDelay() - 1);
 			}else{
 				bullet.fly();
-				if (bullet.getPositionX() < 0 && bullet.getPositionX() >= map.getWidth() &&
-					bullet.getPositionY() < 0 && bullet.getPositionY() >= map.getHeight())
+				int posX = bullet.getPositionX();
+				int posY = bullet.getPositionY();
+				
+				if (posX < 0 && posX >= map.getWidth() &&
+					posY < 0 && posY >= map.getHeight())
 				{
 					bullets.remove(index);
 				}
-				if 
-				bullet.getType()
-				if (map.getElement())
+				
+				boolean collide = false;
+				if ((MapObject obj = map.getElement(posX, posY)) != null) {
+					obj.decreaseHealth(bullet.getType().getDemage());
+					if (obj.getHealth() == 0) {
+						map.setElement(posX, posY, null);
+						mapObjects.remove(obj);
+					}
+					collide = true;
+				}
+				if ((int bnsId = findBonusAt(posX, posY)) != -1) {
+					Bonus bonus = bouses.get(bnsId);
+					bonus.decreaseHealth(bullet.getType().getDemage());
+					if (bonus.getHealth() == 0) {
+						bouses.remove(bnsId);
+					}
+					collide = true;
+				}
+				for (int unitId=units.size()-1; unitId>=0; unitId--) {
+					Unit unit = units.get(unitId);
+					if (unit.getPositionX() == posX && unit.getPositionY() == posY) {
+						unit.decreaseHealth(bullet.getType().getDemage());
+						if (unit.getHealth() == 0) {
+							processDeadUnit(unit);
+						}
+						collide = true;
+					}
+				}
+
+				if (collide)
+					bullets.remove(index);
 			}
 		}
 	}
@@ -114,7 +174,7 @@ class Game {
 			// Do client ACTIONS
 			doUnitActions();
 			// Calc new bullets position
-			proccessBullets();
+			processBullets();
 		}
 
     }
