0ee11a162d8e036ef5c9c81323a668294b25bc6d
dychko <dichko.v@bigmir.net>
2014-11-23 07:30:02 +0200

    Weapon names

--- src/Weapon.java
+++ src/Weapon.java
@@ -2,10 +2,23 @@
  * Created by Victor Dichko on 22.11.14.
  */
 public class Weapon {
+    private String name;
     private int reloadTime;
     private BulletType bulletType;
     private int bulletsNumber;
     private int leftDelayForShot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLeftDelayForShot() {
+        return leftDelayForShot;
+    }
 
     public int getReloadTime() {
         return reloadTime;
