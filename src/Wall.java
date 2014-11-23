9e48ba98e46cc7bb575339ee4387e376a45b63ee
dychko <dichko.v@bigmir.net>
2014-11-23 05:14:40 +0200

    Add walls constructor

--- src/Wall.java
+++ src/Wall.java
@@ -2,9 +2,7 @@
  * Created by Victor Dichko on 22.11.14.
  */
 public class Wall extends MapObject {
-//    public Wall setHorizontalWall(int positionX, int startPositionY, int endPositionY) {
-//        for (int i = startPositionY; i < endPositionY + 1; i++) {
-//
-//        }
-//    }
+    public Wall() {
+        this.setHealth(100);
+    }
 }
